8be9a5695c625f637d0be0af9d262878b9577d47
alexanderlamoureux@hotmail.com <alexanderlamoureux@hotmail.com>
2017-07-16 16:41:00 -0700

    working on this portfolio, slowllyyyyyyy

--- app.js
+++
@@ -1,8 +0,0 @@
-function Portfolio(aboutMe, classProjects, sideProjects, learningJournal) {
-  this.aboutMe = me;
-  this.classProjects = classProjects;
-  this.sideProjects = sideProjects;
-  this.learningJournal = journal;
-}
-var aboutMe = new Portfolio("Alexander", "Lamoureux", 20, "blue");
-var classProjects = new Portfolio("Sally", "Rally", 48, "green");
